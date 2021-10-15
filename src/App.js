55c49472494f84e23738a5866c4b8309d308479b
souravwebart <guptasourav536@gmail.com>
2021-10-15 13:13:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="Login_container">
+        <div className="login_box">
+          <div className="login__header">
+            <div className="login_brand">
+              <img src="/img/logo.png" alt="icon"></img>
+            </div>
+            <div className="login__form">
+              <form className="login_form_input">
+                <div className="form-group login_form_grp">
+                  <img src="/img/mail.png" alt="icon"></img>
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"></input>
+                </div>
+                <div className="form-group login_form_grp">
+                  <img className="password__icon" src="/img/password.png" alt="icon"></img>
+                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                </div>
+                <div className="password-details">
+                  <div className="remember_me">
+                    <img src="/img/Vector.png" alt="icon"></img>
+                    <p>Remember me</p>
+                  </div>
+                  <div className="Forgot_password">
+                  <p>Forgot password?</p>
+                  </div>
+                </div>
+                <button type="submit" className="button-login">Login</button>
+                <div className="signup__details">
+                <p>Don’t have an account? <a href="/">Sign up</a> </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
